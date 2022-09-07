c760b509f8fde54a521688543c51e9c79f71ebfe
Filipp Denisov <denisovphilip@yandex.ru>
2022-09-07 15:57:59 +0400

    728 self dividing numbers (js solution)

---
+++ 728-self-dividing-numbers/main.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} x
+ * @returns {bool}
+ */
+var isSelfDividing = function(x) {
+	var s = x.toString();
+	var n = 0;
+	for (var i = 0; i < s.length; i++) {
+   		n = s[i];
+		if (x == 0 || x % n != 0) {
+			return false
+		}
+ 	}	
+	return true
+};
+
+
+
+/**
+ *
+ * @param {number} left
+ * @param {number} right
+ * @return {number[]}
+ */
+var selfDividingNumbers = function(left, right) {
+	var res = [];
+	for (i = left; i <= right; i ++) {
+		if (isSelfDividing(i)) {
+			res.push(i);
+		}
+	}
+	return res
+};
+
+console.log(selfDividingNumbers(1, 22));
